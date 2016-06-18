b2a866fb219bc09d5ef7e433dbb970f3b9513b0b
sumodkulkarni <passward@22>
2016-06-18 19:20:17 +0530

    HomeFragmentWithTabs using Blunderer's library

---
+++ app/src/main/java/com/sumod/interfaceapp/fragments/HomeFragmentWithTabs.java
@@ -0,0 +1,29 @@
+package com.sumod.interfaceapp.fragments;
+
+import com.blunderer.materialdesignlibrary.handlers.ViewPagerHandler;
+import com.sumod.interfaceapp.R;
+
+
+public class HomeFragmentWithTabs
+        extends com.blunderer.materialdesignlibrary.fragments.ViewPagerWithTabsFragment {
+
+    @Override
+    public ViewPagerHandler getViewPagerHandler() {
+        return new ViewPagerHandler(getActivity())
+                .addPage(R.string.proposals,
+                        ProposalsFragment.newInstance("sampleString", "sampleString"))
+                .addPage(R.string.leads,
+                        LeadsFragment.newInstance("sampleString", "sampleString"));
+    }
+
+    @Override
+    public boolean expandTabs() {
+        return false;
+    }
+
+    @Override
+    public int defaultViewPagerPageSelectedPosition() {
+        return 0;
+    }
+
+}
