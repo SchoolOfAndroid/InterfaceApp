2e84039764f0d311b261a8a0e80ee7251542b6d0
sumodkulkarni <passward@22>
2016-06-18 21:32:47 +0530

    Messages appear in a custom listview

---
+++ app/src/main/java/com/sumod/interfaceapp/fragments/MessagesFragment.java
@@ -0,0 +1,88 @@
+package com.sumod.interfaceapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import com.sumod.interfaceapp.R;
+import com.sumod.interfaceapp.adapters.ChatListAdapter;
+import com.sumod.interfaceapp.adapters.JobListAdapter;
+import com.sumod.interfaceapp.model.Chat;
+import com.sumod.interfaceapp.model.Job;
+
+import java.util.ArrayList;
+
+
+public class MessagesFragment extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    
+    private String mParam1;
+    private String mParam2;
+
+    private ListView listView_chats;
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static MessagesFragment newInstance(String param1, String param2) {
+        MessagesFragment fragment = new MessagesFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        listView_chats = (ListView) view.findViewById(R.id.listView_chats);
+
+        populateListView();
+    }
+
+    protected void populateListView() {
+
+        ArrayList<Chat> chatList = new ArrayList<>();
+
+        chatList.add(new Chat(0, "User 0", "20:28"));
+        chatList.add(new Chat(1, "User 1", "19:28"));
+        chatList.add(new Chat(2, "User 2", "18:28"));
+        chatList.add(new Chat(3, "User 3", "17:28"));
+        chatList.add(new Chat(4, "User 4", "16:28"));
+        chatList.add(new Chat(5, "User 5", "15:28"));
+
+        ChatListAdapter myChatListAdapter = new ChatListAdapter(getContext(), chatList);
+
+        listView_chats.setAdapter(myChatListAdapter);
+
+    }
+
+}
